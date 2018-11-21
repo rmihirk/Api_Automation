8090531142aedafc686bd6507f4ab76e4a3c10e5
Make <mihir.rathod23@gmail.com>
2018-11-21 10:55:19 +0530

    Update serverconf.js

--- Configuration/serverconf.js
+++ Configuration/serverconf.js
@@ -2,4 +2,4 @@ var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('apiConfig.properties');
 var supertest = require('supertest');
 
-module.exports.baseURL = supertest.agent(properties.get('baseURL4core'));
+module.exports.baseURL = supertest.agent(properties.get('baseURL'));
